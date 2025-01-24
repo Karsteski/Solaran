d250f138d251e8dc8ba0e07cfdd0c07479687f77
Kareem Skinner <kareem@karsteski.com>
2025-01-24 13:18:33 -0500

    Added basic subject and test map behaviour

---
+++ maps/test_map.gd
@@ -0,0 +1,21 @@
+@tool
+extends Node3D
+
+
+@onready var spawn_point: Node3D = $world/spawn_point
+
+@export var character: PackedScene = preload("res://characters/test_subject.tscn")
+
+
+func spawn_character() -> void:
+	spawn_point.add_child(character.instantiate())
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_character()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
